f1061d639d7b16ffd17db77ea32d964c97b62ec9
Shekul-Islam <shekul.bd201@gmail.com>
2024-03-05 19:04:13 +0600

    update

---
+++ PHP/php-basic/mid_evidence_php2/find_prime.php
@@ -0,0 +1,29 @@
+<form method='post'>
+enter a number<input type="text" name="fname">
+<input type="submit" name="submit" value='submit'>
+</form>
+
+
+
+
+<?php
+$b=$_POST["fname"];
+$c=0;
+if(isset($_POST["submit"]))
+{
+if($b==0 || $b==1)
+{
+  echo "Not Prime or Composite";
+  exit();
+}
+for($i=2;$i<$b;$i++)
+{
+  if($b%$i==0)
+  $c++;
+}
+if($c==0)
+// echo '<script>alert("prim number")</script>';
+echo "$b is Prime number";
+else
+  echo "$b is not a prime number";
+}?>
