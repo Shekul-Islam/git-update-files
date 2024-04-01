484dad7a98a378fbcd810afac60686fb8e533ca0
Md. Shekul Islam <shekul.bd201@gmail.com>
2024-04-02 02:12:03 +0600

    update

--- database.php
+++ database.php
@@ -65,5 +65,55 @@ $connectwithdb = new mysqli('localhost','root','','company');
     <form action="#" method="post">
         
     </form>
+
+<!-- 
+    <form action="#" method="post">
+	<table>
+		<tr>
+			<td><label for="manufac">Manufacturer</label></td>
+			<td>
+				<select name="manufac">
+					<?php 
+						// $manufac = $db->query("select * from manufacturer");
+						// while(list($_mid,$_mname) = $manufac->fetch_row()){
+						// 	echo "<option value='$_mid'>$_mname</option>";
+						// }
+					?>
+				</select>
+			</td>
+		</tr>
+		<tr> 
+			<td></td>
+			<td><input type="submit" name="delmanufact" value="delete" /></td>
+		</tr>
+	</table>
+</form>
+
+
+
+<h3>View Product</h3>
+
+<table border="1" style="border-collapse: collapse;" > 
+	<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Price</th>
+		<th>Manufacturer</th>
+		<th>Contact</th>
+	</tr>
+	<?php 
+		// $product = $db->query(" select * from view_product ");
+		// while(list($_id,$_name,$_price,$_mname,$_mcont) = $product->fetch_row()){
+		// 	echo "<tr> 
+		// 			<td>$_id</td>
+		// 			<td>$_name</td>
+		// 			<td>$_price</td>
+		// 			<td>$_mname</td>
+		// 			<td>$_mcont</td>
+		// 		</tr>";
+		// }
+	
+	?> -->
+</table>
 </body>
 </html>
